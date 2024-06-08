47fe87aabf4589facc24a23015f60120ef295e89
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-06-08 15:15:54 +0900

    Title: 십진수와 이진수, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240608/십진수와 이진수/decimal-and-binary-number.java
+++ 240608/십진수와 이진수/decimal-and-binary-number.java
@@ -1,10 +1,17 @@
 import java.util.*;
+import java.math.*;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        int t = Integer.parseInt(input,2) * 17;
-        System.out.println(Integer.toString(t,2));
+        Scanner scanner = new Scanner(System.in);
+        String binaryInput = scanner.nextLine();
+        BigInteger decimalValue = new BigInteger(binaryInput, 2);
+        BigInteger multipliedValue = decimalValue.multiply(BigInteger.valueOf(17));
+
+        // 십진수를 이진수로 변환
+        String resultBinary = multipliedValue.toString(2);
+
+        // 결과 출력
+        System.out.println(resultBinary);
     }
 }
