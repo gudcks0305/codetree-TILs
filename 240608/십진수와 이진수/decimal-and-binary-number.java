3579d4568567e03205a45429106a66f879a8156e
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-06-08 15:12:39 +0900

    Title: 십진수와 이진수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240608/십진수와 이진수/decimal-and-binary-number.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        int t = Integer.parseInt(input,2) * 17;
+        System.out.println(Integer.toString(t,2));
+    }
+}
