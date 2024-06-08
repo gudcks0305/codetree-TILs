a4db9ccf459c5edcb4442922f14afa40db5266db
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-06-08 15:43:34 +0900

    Title: 알파벳 회전초밥, Time: 110ms, Memory: 8MB, Status: Passed - CodeTree

--- 240608/알파벳 회전초밥/alphabet-rotating-sushi.java
+++ 240608/알파벳 회전초밥/alphabet-rotating-sushi.java
@@ -1,21 +1,33 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        String wA = sc.nextLine();
-        String wB = sc.nextLine();
-        int result = 1;
-        int index = 0;
-        for(char bChar : wB.toCharArray()){
-            int bCharIndex = wA.indexOf(bChar);
-            if(index <= bCharIndex){
-                index = bCharIndex;
-                result++;
-            }else{
-                index = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        
+        int count = 1;  // a의 반복 횟수
+        String str = a; // 비교할 문자열
+        int idx;        // 부분 문자열 인덱스
+
+        for (int i = 0; i < b.length(); i++) {
+            // 현재 단어
+            String target = String.valueOf(b.charAt(i));
+
+            // 비교할 문자열에 포함되어 있으면 부분문자열 구하기
+            if (str.contains(target)) {
+                idx = str.indexOf(target) + 1;
+                str = str.substring(idx);
+            // 없으면 count 증가 후 부분문자열 구하기
+            } else {
+                idx = a.indexOf(target) + 1;
+                str = a.substring(idx);
+                count++;
             }
         }
-        System.out.println(result);
+
+        System.out.println(count);
     }
 }
