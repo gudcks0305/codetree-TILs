619d27cef5cc425a0c654127c1bb4fbc8c7d17d1
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-08-08 22:44:48 +0900

    Title: -999, Time: 247ms, Memory: 15MB, Status: Passed - CodeTree

--- 240808/-999/-999.java
+++ 240808/-999/-999.java
@@ -4,11 +4,18 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-   public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int index999 = -1;
+        for (int i = 0; i < inputs.length; i++) {
+            if (inputs[i] == -999) {
+                index999 = i;
+                break;
+            }
+        }
+        inputs = Arrays.copyOfRange(inputs, 0, index999);
         Arrays.sort(inputs);
-        System.out.println(inputs[inputs.length - 1] + " " + inputs[1]);
-
+        System.out.println(inputs[inputs.length - 1] + " " + inputs[0]);
     }
 }
