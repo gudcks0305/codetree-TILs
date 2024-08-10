c40097ee42e36bebd80b9f97709b175cd45f64c7
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-08-10 21:57:11 +0900

    Title: 제곱수의 합으로 나타내기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240810/제곱수의 합으로 나타내기/express-as-sum-of-squares.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int dp[];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        ans(n);
+        System.out.println(dp[n]);
+
+    }
+    public static void ans(int n) {
+
+
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            int min=100001;
+
+            for(int j=1; j<= Math.sqrt(i); j++) {
+
+                if(j*j == i) { // i가 어떤수의 제곱인 경우는 dp가 1이다
+                    min = 1;
+                    break;
+                }
+                else{
+                    min = Math.min(min, dp[j]+dp[i-j]);
+                }
+
+            }
+            dp[i] = min;
+            //System.out.println("dp["+i + "] == " + min);
+        }
+
+        return;
+
+    }
+}
