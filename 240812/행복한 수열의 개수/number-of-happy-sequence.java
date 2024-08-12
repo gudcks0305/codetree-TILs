180dbe9daa32a94958c2c38d8ac15ef2988f65bc
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-08-12 22:21:15 +0900

    Title: 행복한 수열의 개수, Time: 246ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240812/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] maps = new int[n][n];
+        for(int i = 0; i < n; i++){
+            maps[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray(); 
+        }
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            if(isHappy(i,m,maps)){
+                cnt++;
+            }
+            if(isHappyY(i,m,maps)){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+        br.close();
+    }
+
+    public static boolean isHappy(int x,int m,int[][] maps){
+        int cnt = 1;
+        int first = maps[0][x];
+        for(int i = 1; i < maps.length; i++){
+            if(maps[i][x] == first) {
+                cnt++;
+            }else{
+                cnt=1;
+            }
+            first = maps[i][x];
+
+        }
+        return cnt >= m;
+    }
+
+    public static boolean isHappyY(int y,int m,int[][] maps){
+        int cnt = 1;
+        int first = maps[y][0];
+        for(int i = 1; i < maps.length; i++){
+            if(maps[y][i] == first) {
+                cnt++;
+
+            }else{
+                cnt=1;
+            }
+            first = maps[y][i];
+
+        }
+        return cnt >= m;
+    }
+}
