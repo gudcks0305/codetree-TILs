3d59d4927273cedf4712250874f6e5ad4df73330
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-08-25 15:29:24 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 168ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240825/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 240825/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -26,15 +26,25 @@ public class Main {
     static final int[] dy = {1,-1,0,0};
     static final int[] dx = {0,0,1,-1};
     public static final void dfs(int[][] maps , boolean[][] visited, int y , int x){
+        print(visited);
         for(int i = 0 ; i < 4 ; i++){
             int nextY = y + dy[i];
             int nextX = x + dx[i];
             if(nextY < 0|| nextY > maps.length - 1 || nextX < 0 || nextX > maps[y].length - 1){
                 continue;
             }
-            if(visited[nextY][nextX] || maps[nextY][nextX] == 1) continue;
+            if(visited[nextY][nextX] || maps[nextY][nextX] == 0) continue;
             visited[nextY][nextX] = true;
-            dfs(maps,visited,y,x);
+            dfs(maps,visited,nextY,nextX);
+        }
+    }
+    static void print(boolean[][] visited){
+        System.out.println();
+        for(int i = 0; i < visited.length; i++){
+            for(int j = 0; j < visited[i].length; j++){
+                System.out.print(visited[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
