8e61a96ff067e1921bebc750d5a949cfde1515e6
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2024-08-27 22:35:57 +0900

    Title: 뿌요뿌요, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240827/뿌요뿌요/puyo-puyo.java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class Main {
+    static int max = 0;
+    static int cnt = 0;
+    static int value = 0;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] maps = new int[n][n];
+        boolean[][] visited = new boolean[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n ; j++){
+                maps[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n; j++){
+                if(!visited[i][j]){
+                    cnt = 0;
+                    dfs(maps,visited,i,j);
+                    max = Math.max(max,cnt);
+                    if(cnt >= 4){
+                        value = maps[i][j];
+                    }
+                    
+                }
+            }
+        }
+        System.out.println(value + " " + max);
+    }
+    static int[] dy = {1,-1,0,0};
+    static int[] dx = {0,0,1,-1};
+    public static void dfs(int[][] maps,boolean[][] visited,int y , int x){
+        for(int i = 0; i < 4; i++){
+            int nextY = y + dy[i];
+            int nextX = x + dx[i];
+            if(nextY < 0 || nextY > maps.length - 1 || nextX < 0 || nextX > maps.length - 1){
+                continue;
+            }
+            if(visited[nextY][nextX]){
+                continue;
+            }
+            if(maps[y][x] != maps[nextY][nextX]) continue;
+            visited[nextY][nextX] = true;
+            cnt++;
+            dfs(maps,visited,nextY,nextX);
+        }
+    }
+}
